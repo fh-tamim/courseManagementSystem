ea7de4d985cef0cd09d9acd8423836645afc7b07
anikasultana66 <91877395+anikasultana66@users.noreply.github.com>
2021-10-24 13:31:03 +0600

    Add files via upload
    initial commit

---
+++ java/database/Database.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Database {
+
+    private final static String className = "oracle.jdbc.driver.OracleDriver";
+    private final static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private final static String user = "system";
+    private final static String password = "tiger";
+    private static Connection connection;
+
+    
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName(className);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
